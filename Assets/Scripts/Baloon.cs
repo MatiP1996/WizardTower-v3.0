d7f8a203e3ace4b0abaa53e3ccc731a9224dfd2d
rayEdward <edwardm488@gmail.com>
2022-04-28 22:43:55 +0100

    a
    a

--- Assets/Scripts/Baloon.cs
+++ Assets/Scripts/Baloon.cs
@@ -7,12 +7,7 @@ public class Baloon : InteractionParent
     // Start is called before the first frame update
     private void Start()
     {
-        firstMessage = "E - Launch Baloon";
-    }
 
-    public override List<int> Activate(List<int> playerItems)
-    {
-        return playerItems;
     }
     // Update is called once per frame
     void Update()
